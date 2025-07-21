4369ec3827389bd4d44b219735aafb2008967e7b
Joeweh <joeyqsa@outlook.com>
2025-07-21 14:49:58 -0400

    Initial Commit

---
+++ android/app/src/main/kotlin/io/github/joeweh/filler_word_recognition/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.github.joeweh.filler_word_recognition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Filler Word Recognition',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
